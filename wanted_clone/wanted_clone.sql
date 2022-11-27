ccacdfa2ab238decf97297bfb405714d88c83c2b
hoilic <115633485+hoilic@users.noreply.github.com>
2022-11-27 17:21:44 +0900

    jobDB 업뎃 및 xlsx 파일 추가

--- wanted_clone/wanted_clone.sql
+++ wanted_clone/wanted_clone.sql
@@ -12,3 +12,22 @@ seq varchar(30));
 insert into job values ('1','kor','coupang','한국/서울','10000',sysdate,jobseq.nextval);
 
 commit;
+
+create table job(
+img varchar2(60),
+subject varchar2(80),
+company varchar2(30),
+location varchar2(30),
+content varchar2(15000),
+reward varchar2(30),
+companylocation varchar2(300),
+seq varchar2(30));
+
+select*from jobtest;
+
+drop table jobtest;
+drop table job;
+
+select*from job;
+
+
